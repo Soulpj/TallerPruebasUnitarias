72e93c7459b7f4794b7870cf9cee2a473fb9fa3e
fm116081 <franciscoj.mejia@bch.hn>
2026-05-13 16:09:30 -0600

    dia2

---
+++ test/Course.UnitTest/CustomerTests.cs
@@ -0,0 +1,39 @@
+﻿using Course.Domain.Entities;
+using Course.Domain.Exceptions;
+using FluentAssertions;
+
+namespace Course.UnitTest;
+
+public class CustomerTests
+{
+    [Fact]
+    public void Constructor_WhenDataIsValid_ShouldCreateCustomer()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+
+        // Act
+        Customer customer = new(id, "John Doe", "john.doe@bch.hn");
+
+
+        Assert.Equal(id, customer.Id);
+        Assert.NotEqual(Guid.Empty, customer.Id);
+        Assert.Equal("John Doe", customer.FullName);
+        Assert.Equal("john.doe@bch.hn", customer.Email);
+    }
+
+    [Fact]
+    public void Constructor_WhenEmailIsInvalid_ShouldThrowDomainException()
+    {
+        // Arrange Act
+        
+        var act = () => new Customer(Guid.NewGuid(), "John Doe", "JohnDoe");
+
+        
+        act.Should().Throw<DomainException>().WithMessage("El cliente requiere un correo valido.");
+
+    }
+
+
+
+}
---
+++ test/Course.UnitTest/OrderTests.cs
@@ -0,0 +1,47 @@
+﻿using Course.Application.Abstractions;
+using Course.Application.Orders;
+using Course.Domain.Entities;
+using Course.Domain.Enums;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.UnitTest;
+
+public class OrderTests
+{
+    private readonly Mock<ICustomerRepository> _customers = new();
+    private readonly Mock<IProductRepository> _products = new();
+    private readonly Mock<IOrderRepository> _orders = new();
+    private readonly Mock<IPaymentGateway> _payments= new();
+
+    private readonly OrderService _service;
+    public OrderTests()
+    {
+        _service = new OrderService(
+            _customers.Object,
+            _products.Object,
+            _orders.Object,
+            _payments.Object);
+    }
+
+    [Fact]
+    public async Task Create_WhenOrderIsValid_ShouldCreateOrder() { 
+
+        // Arrange 
+        var customerId = Guid.NewGuid();
+        var productId = Guid.NewGuid();
+
+        var itemsRequest = new List<CreateOrderItemRequest>();
+        itemsRequest.Add(new CreateOrderItemRequest ( productId, 2 ));
+
+        var request = new CreateOrderRequest(customerId, itemsRequest);
+
+        var customer = new Customer(customerId, "John Doe", "john.doe@bch.hn");
+        var product = new Product(productId, "Laptop", 1000, 10);
+        var payment = new Payment(Guid.NewGuid(), Guid.NewGuid(), 2000, PaymentStatus.Approved, "");
+
+        // Act
+    }
+}
