92b8dcfb854437e4e5803236c1bb6ebae4e05ec6
fm116081 <franciscoj.mejia@bch.hn>
2026-05-14 16:06:42 -0600

    dia 2 subido

--- test/Course.UnitTest/OrderTests.cs
+++ test/Course.UnitTest/OrderTests.cs
@@ -2,6 +2,7 @@
 using Course.Application.Orders;
 using Course.Domain.Entities;
 using Course.Domain.Enums;
+using FluentAssertions;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -40,8 +41,44 @@ public class OrderTests
 
         var customer = new Customer(customerId, "John Doe", "john.doe@bch.hn");
         var product = new Product(productId, "Laptop", 1000, 10);
-        var payment = new Payment(Guid.NewGuid(), Guid.NewGuid(), 2000, PaymentStatus.Approved, "");
+        //var payment = new Payment(Guid.NewGuid(), Guid.NewGuid(), 2000, PaymentStatus.Approved, "");
+
+        _customers.Setup(x => x.GetByIdAsync(customerId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(customer);
+
+        _products.Setup(x => x.GetByIdAsync(productId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(product);
+
+        _payments.Setup(x => x.PayAsync(It.IsAny<Guid>(),2000, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Guid orderId, decimal amount, CancellationToken _) => 
+            new Payment(Guid.NewGuid(), orderId, amount, PaymentStatus.Approved, "")
+            );
+
+        
 
         // Act
+
+        var response = await _service.CreateAsync(request);
+
+        // Assert
+        
+        response.Should().NotBeNull();
+        response.CustomerId.Should().Be(customerId);
+        response.Total.Should().Be(2000);
+
+        //response.Items.Should().HaveCount(1);
+        //response.Items[0].ProductId.Should().Be(productId);
+        //response.Items[0].Quantity.Should().Be(2);
+
+        _orders.Verify(x => x.AddAsync
+        (
+            It.Is<Order>(o => o.CustomerId == customerId &&
+            o.Total == 2000 &&
+            o.Status == OrderStatus.Paid &&
+            o.Payment != null &&
+            o.Payment.OrderId == o.Id),
+            It.IsAny<CancellationToken>()), Times.Once
+        );
+
     }
 }
